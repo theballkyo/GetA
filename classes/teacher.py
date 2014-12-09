88cbf4c93c4200f67fc6d9a27e2b0d86e0ff5bac
theballkyo <s7070148@kmitl.ac.th>
2014-12-10 01:13:24 +0700

    Teacher Funtion

--- classes/teacher.py
+++ classes/teacher.py
@@ -1,7 +1,46 @@
 class Teacher():
 
     def __init__(self):
-        print (type(self.root))
+        self.member = int(input())
+        self.data = []
+        self.a = int(input())
+        self.bp = int(input())
+        self.b = int(input())
+        self.cp = int(input())
+        self.c = int(input())
+        self.dp = int(input())
+        self.d = int(input())
+        for i in range(self.member):
+            self.data.append([int(input()), i+1])
+        self.data.sort(reverse = True)
 
     def initUI(self):
         pass
+    
+    def resault(self):
+        for i in range(len(self.data)):
+            print ("No."+ str(self.data[i][1])+" Score : "+str(self.data[i][0])),
+            if self.a != 0:
+                print ("Grade is A")
+                self.a -= 1
+            elif self.bp != 0:
+                print ("Grade is B+")
+                self.bp -= 1
+            elif self.b != 0:
+                print ("Grade is B")
+                self.b -= 1
+            elif self.cp != 0:
+                print ("Grade is C+")
+                self.cp -= 1
+            elif self.c != 0:
+                print ("Grade is C")
+                self.c -= 1
+            elif self.dp != 0:
+                print ("Grade is D+")
+                self.dp -= 1
+            elif self.d != 0:
+                print ("Grade is D")
+                self.d -= 1
+            else:
+                print ("Grade is F")
+            
