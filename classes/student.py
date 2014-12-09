1203d139a190b1511b8e6bf0ebbc747dfbaf4a69
Thanakrit Pintana <kh4z1234@gmail.com>
2014-12-10 03:33:16 +0700

    serepate mid and final

--- classes/student.py
+++ classes/student.py
@@ -25,7 +25,7 @@ class Student:
         """ Create UI for add new subject """
         self.top = self.tk.Toplevel()
         self.top.title("Add new subject")
-        self.top.geometry("300x200")
+        self.top.geometry("300x220")
         self.top.resizable(0, 0)
         self.top.focus()
 
@@ -36,7 +36,7 @@ class Student:
         self.e = self.tk.Entry(frame, width=10)
         self.e.grid(row=0, column=1, padx=5, pady=5)
         self.e.focus()
-        #self.e.bind("<Return>", self.add_subject)
+        self.e.bind("<Return>", self.add_subject)
         
         self.tk.Message(frame, text="Weight :", width=50).grid(row=0, column=2)
         self.weight = self.tk.Entry(frame, width=3)
@@ -45,33 +45,39 @@ class Student:
         self.tk.Message(frame, text="Score", width=50).grid(row=1, column=1)
         self.tk.Message(frame, text="Max", width=50).grid(row=1, column=2)
         
-        self.tk.Message(frame, text="Exam :", width=50).grid(row=2, column=0)
-        self.s_exam = self.tk.Entry(frame, width=5)
-        self.s_exam.grid(row=2, column=1)
-        self.b_exam = self.tk.Entry(frame, width=5)
-        self.b_exam.grid(row=2, column=2)
+        self.tk.Message(frame, text="Midterm :", width=80).grid(row=2, column=0)
+        self.s_exam_mid = self.tk.Entry(frame, width=5)
+        self.s_exam_mid.grid(row=2, column=1)
+        self.b_exam_mid = self.tk.Entry(frame, width=5)
+        self.b_exam_mid.grid(row=2, column=2)
 
-        self.tk.Message(frame, text="Project :", width=50).grid(row=3, column=0)
+        self.tk.Message(frame, text="Final :", width=50).grid(row=3, column=0)
+        self.s_exam_final = self.tk.Entry(frame, width=5)
+        self.s_exam_final.grid(row=3, column=1)
+        self.b_exam_final = self.tk.Entry(frame, width=5)
+        self.b_exam_final.grid(row=3, column=2)
+
+        self.tk.Message(frame, text="Project :", width=50).grid(row=4, column=0)
         self.s_project = self.tk.Entry(frame, width=5)
-        self.s_project.grid(row=3, column=1)
+        self.s_project.grid(row=4, column=1)
         self.b_project = self.tk.Entry(frame, width=5)
-        self.b_project.grid(row=3, column=2)
+        self.b_project.grid(row=4, column=2)
 
-        self.tk.Message(frame, text="Homework :", width=80).grid(row=4, column=0)
+        self.tk.Message(frame, text="Homework :", width=80).grid(row=5, column=0)
         self.s_hw = self.tk.Entry(frame, width=5)
-        self.s_hw.grid(row=4, column=1)
+        self.s_hw.grid(row=5, column=1)
         self.b_hw = self.tk.Entry(frame, width=5)
-        self.b_hw.grid(row=4, column=2)
+        self.b_hw.grid(row=5, column=2)
 
-        self.tk.Message(frame, text="Other :", width=50).grid(row=5, column=0)
+        self.tk.Message(frame, text="Other :", width=50).grid(row=6, column=0)
         self.s_other = self.tk.Entry(frame, width=5)
-        self.s_other.grid(row=5, column=1)
+        self.s_other.grid(row=6, column=1)
         self.b_other = self.tk.Entry(frame, width=5)
-        self.b_other.grid(row=5, column=2)
+        self.b_other.grid(row=6, column=2)
         
         button_ok = self.tk.Button(frame, text='OK', command=self.add_subject)
-        button_ok.grid(row=6, column=1)
-        buttun_clo = self.tk.Button(frame, text='Close', command=self.top.destroy).grid(row=6, column=2)
+        button_ok.grid(row=7, column=1)
+        buttun_clo = self.tk.Button(frame, text='Close', command=self.top.destroy).grid(row=7, column=2)
 
     def set_e_text(self, text):
         """ Set Entry Text """
