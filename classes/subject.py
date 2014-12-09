a370b7d0651ae08ee7d4acb4208499337dee1592
theballkyo <theballkyo@gmail.com>
2014-12-10 03:55:49 +0700

    New teacher UI

--- classes/subject.py
+++ classes/subject.py
@@ -1,28 +1,40 @@
-from classes.grade import *
-
 class Subject():
-    def __init__(self, tk, root, name):
 
+    def __init__(self, parent, name):
         self.name = name
-        self.tk = tk
-        self.root = root
+        self.tk = parent.tk
+        self.root = parent.root
+        # # New Frame
+        # self.frame = tk.Frame(root)
+        # self.frame.pack()
+
+        # # Create 2 Button
+        # self.b = tk.Button(self.frame, text=name, command=self.print_box)
+        # self.rm = tk.Button(self.frame, text="X", command=self.remove)
+
+        # # Config grid
+        # self.b.grid(row=0, column=0)
+        # self.rm.grid(row=0, column=1)
+
+    def teacher_initUI(self):
         # New Frame
-        self.frame = tk.Frame(root)
+        self.frame = self.tk.Frame(self.root)
         self.frame.pack()
 
         # Create 2 Button
-        self.b = tk.Button(self.frame, text=name, command=self.print_box)
-        self.rm = tk.Button(self.frame, text="X", command=self.remove)
+        self.b = self.tk.Button(self.frame, text=self.name, command=self.print_box, width=50)
+        self.rm = self.tk.Button(self.frame, text="Remove", command=self.remove)
 
         # Config grid
-        self.b.grid(row=0, column=0)
-        self.rm.grid(row=0, column=1)
+        self.b.grid(row=0, column=0, padx=5, pady=5)
+        self.rm.grid(row=0, column=1, padx=5, pady=5)
 
     def print_box(self):
-        self.g = Grade(self)
+        pass
+        # self.g = Grade(self)
 
     def remove(self):
         self.frame.destroy()
 
     def get_text(self):
-        return self.b["text"]
+        return self.name
--- classes/teacher.py
+++ classes/teacher.py
@@ -1,4 +1,4 @@
-from classes.student_class.subject import *
+from classes.subject import *
 
 class Teacher():
 
@@ -51,7 +51,7 @@ class Teacher():
         self.e = self.tk.Entry(frame, width=10)
         self.e.grid(row=0, column=1, padx=5, pady=5)
         self.e.focus()
-        #self.e.bind("<Return>", self.add_subject)
+        self.e.bind("<Return>", self.add_subject)
         
         self.tk.Message(frame, text="Score").grid(row=1, column=0)
         self.in_score = self.tk.Entry(frame, width=5)
@@ -82,8 +82,10 @@ class Teacher():
             return False
 
         # New object subject
-        btn = Subject(self.tk, self.root, name)
-        self.SUBJECT_LIST.append(btn)
+        sbj = Subject(self, name)
+        sbj.teacher_initUI()
+
+        self.SUBJECT_LIST.append(sbj)
         self.tk.messagebox.showinfo(message="Success - " + str(name), title="Success")
         self.set_e_text("")
         self.get_subject()
@@ -132,7 +134,7 @@ class Teacher():
         self.e.grid(row=0, column=0, padx=5, pady=15)
         self.e.focus()
         self.e.bind("<Return>", self.add_subject)
-        
+
         button_ok = self.tk.Button(frame, text='OK', command=self.add_subject)
         button_ok.grid(row=0, column=1)
         buttun_clo = self.tk.Button(frame, text='Close', command=self.top.destroy).grid(row=0, column=2)
